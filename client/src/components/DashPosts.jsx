7dcc7dcd2824e99f2858955d5a94ab11d7f9c24d
sganjare29 <sganjare29@gmail.com>
2026-06-15 16:16:08 +0530

    added 'Show more' in dashboard posts.

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const { currentUser } = useSelector(state => state.user);
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
+
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -25,18 +27,36 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
             <TableHead>
-              <TableHeadCell>Date Updated</TableHeadCell>
-              <TableHeadCell>Post Image</TableHeadCell>
-              <TableHeadCell>Post Title</TableHeadCell>
-              <TableHeadCell>Category</TableHeadCell>
-              <TableHeadCell>Delete</TableHeadCell>
-              <TableHeadCell><span>edit</span></TableHeadCell>
+              <TableRow>
+                <TableHeadCell>Date Updated</TableHeadCell>
+                <TableHeadCell>Post Image</TableHeadCell>
+                <TableHeadCell>Post Title</TableHeadCell>
+                <TableHeadCell>Category</TableHeadCell>
+                <TableHeadCell>Delete</TableHeadCell>
+                <TableHeadCell><span>edit</span></TableHeadCell>
+              </TableRow>
             </TableHead>
             <TableBody className='divide-y'>
               {userPosts.map((post) => (
@@ -54,7 +74,7 @@ export default function DashPosts() {
                       />
                     </Link>
                   </TableCell>
-                   <TableCell>
+                  <TableCell>
                     <Link className='font-medium text-gray-900 dark:text-white ' to={`/post/${post.slug}`}>
                       {post.title}
                     </Link>
@@ -78,10 +98,18 @@ export default function DashPosts() {
               ))}
             </TableBody>
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet!</p>
-      )}
-    </div>
+      )
+      }
+    </div >
   )
 }
